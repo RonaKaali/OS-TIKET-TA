55b057128ad8d12cc8276786484c4227629778df
alexander18-code <ronafatahilah@gmail.com>
2026-05-06 14:58:25 +0800

    Enable error display for debugging

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Forward Vercel requests to normal index.php
 if (isset($_SERVER['VERCEL_URL'])) {
